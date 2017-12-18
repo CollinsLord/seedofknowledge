7b6626c10c47ea7c84cca416b21aeb54deeb7dc7
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2017-12-18 18:45:40 +0000

    Squashed old migrations and new course model. Refs #19

--- courses/models.py
+++ courses/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 from django.urls import reverse 
-
-# Create your models here.
+from subjects.models import Subject
 
 class Course(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
