68b564157e13d606c9efed4d8a44c8b6f6209d43
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2017-12-19 14:00:20 +0000

    Manage units. Refs #15

--- subjects/admin.py
+++ subjects/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Subject
+from .models import Subject, Unit
 
 admin.site.register(Subject)
+admin.site.register(Unit)
--- subjects/models.py
+++ subjects/models.py
@@ -6,3 +6,14 @@ class Subject(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Unit(models.Model):
+    number = models.IntegerField()
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("number", "subject")
+        ordering = ["subject", "number"]
+
